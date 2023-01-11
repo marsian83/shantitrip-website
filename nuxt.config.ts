dbc235661ab3e94d3c34c07fe99f5491bb9f71cb
marsian8 <spanarve@gmail.com>
2023-01-11 11:38:36 +0530

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
-  target:'static',
   app: {
     head: {
       title: "ShantiTrip Holidays",
