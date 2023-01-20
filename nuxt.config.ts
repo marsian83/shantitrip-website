586d6099458398318a09ccd776f858758638d2b2
marsian8 <spanarve@gmail.com>
2023-01-21 02:33:46 +0530

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss"],
+  plugins :[{ src: '~/plugins/vercel.js', mode: 'client' }],
   app: {
     head: {
       title: "ShantiTrip Holidays",
