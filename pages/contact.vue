c1b7fe16df5b9fa96ed5d87db3bcfaaa017519fa
marsian8 <spanarve@gmail.com>
2023-01-20 19:39:11 +0530

    changes

--- pages/contact.vue
+++ pages/contact.vue
@@ -100,7 +100,7 @@
       transparent,
       rgba(var(--text-primary), 0.83)
     ),
-    url("/images/trips.webp");
+    url("/images/gallery/contact.webp");
     text-shadow: 0px 0px 8px rgba(var(--text-primary), 0.69);
 }
 input,
